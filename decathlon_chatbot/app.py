fc218c7156cf81a4522f6e5cb014bd747d52af50
Hamza <hamza.lbelghiti@gmail.com>
2023-05-27 20:51:44 +0100

    Chatbot & Sentiment Analysis on Reviews

---
+++ decathlon_chatbot/app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from chatbot import DecathlonChatbot
+
+st.set_page_config(page_title="DecathlonChat - AI Customer Assistant", page_icon='💬')
+
+with st.sidebar:
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Decathlon_Logo.png/1200px-Decathlon_Logo.png")
+    st.markdown('# 💬 DecathlonChat 🤖')
+    st.markdown('''
+    ---
+    ### 🔮 À Propos
+
+    Decathlon est une entreprise française de grande distribution de sport et de loisirs.
+
+    Avec plus de 2 193 magasins répartis dans 57 pays, le groupe estime son chiffre d’affaires
+    global à plus de 12,4 milliards d'euros annuellement en 2019.
+
+    🏀🏓🏈🎳⚾🏒🥊⛳🤿🏏🎾🎿🏐⛸️
+
+    Cette application est un chatbot qui répond aux questions des clients de Decathlon Maroc en se basant sur les informations disponibles
+    sur le site web [Decathlon Maroc](https://www.decathlon.ma/).
+
+    ---
+    ''')
+
+    st.markdown('😺 See Repository on [Github](https://github.com/Hamagistral/decathlon-chatbot)')
+    st.markdown('👨‍💻 Made by [Hamagistral](https://www.linkedin.com/in/hamza-elbelghiti/)')
+    st.markdown('---')
+    st.markdown("🔴 N.B: Ce projet est une projet personnel et n'est pas affilié à Decathlon Maroc")
+
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["👋🏻 Bonjour! Je suis DecathlonChat, comment-puis-je vous aider ?"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['Salut !']
+
+# Layout of input/response containers
+input_container = st.container()
+colored_header(label='', description='', color_name='blue-30')
+response_container = st.container()
+
+
+def get_text():
+    question = st.text_input("Vous : ", "", key="input")
+    return question
+
+
+with input_container:
+    st.markdown("### Avez-vous une question pour Decathlon Maroc ?")
+    user_input = get_text()
+
+
+def generate_response(prompt):
+    chatbot = DecathlonChatbot()
+    db = chatbot.get_db_decathlon()
+    response = chatbot.get_response_from_query(db, prompt)
+    return response
+
+## Conditional display of AI generated responses as a function of user provided prompts
+with response_container:
+    if user_input:
+        response = generate_response(user_input)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response)
+        
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i), avatar_style="bottts-neutral", seed=90)
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user', avatar_style="avataaars-neutral", seed=10)
+
+
---
+++ decathlon_chatbot/chatbot.py
@@ -0,0 +1,80 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Pinecone
+from langchain.chains import LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from dotenv import find_dotenv, load_dotenv
+import streamlit as st
+import pinecone
+
+OPENAI_API_KEY = st.secrets.openai_api_key
+PINECONE_API_KEY = st.secrets.pinecone_api_key
+PINECONE_ENV = st.secrets.pinecone_env_key
+PINECONE_INDEX = st.secrets.pinecone_index
+
+class DecathlonChatbot:
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        self.embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+    def get_db_decathlon(_self):
+        pinecone.init(
+            api_key=PINECONE_API_KEY, 
+            environment=PINECONE_ENV  
+        )
+
+        index_name=PINECONE_INDEX
+
+        db = Pinecone.from_existing_index(index_name, _self.embeddings)
+
+        return db
+
+    @st.cache_data(show_spinner=False)
+    def get_response_from_query(_self, _db, query, k=4):
+        """
+        Function that generates a response to a customer question using the gpt-3.5-model and the docs provided
+        """
+
+        docs = _db.similarity_search(query, k=k)
+        docs_page_content = " ".join([d.page_content for d in docs])
+
+        chat = ChatOpenAI(model_name="gpt-3.5-turbo")
+
+        # Template to use for the system message prompt
+        template = """
+            Tu es un assistant utile qui peut répondre aux questions des clients sur une plateforme de commerce électronique qui vend des équipements de sport
+            nommé Decathlon basé sur ces données : {docs}. Tu agis comme un chatbot aussi, tu réponds aux phrases des utilisateurs comme "Merci", "Bonjour" etc...
+           
+            D'abord tu classes le sentiment de la question ou de la phrase du client, et tu utilises uniquement les informations données précédemment pour répondre à la question,
+            et tu réponds en prenant compte du sentiment du client.
+           
+            Si tu n'as pas assez d'informations ou tu n'as pas trouvé d'informations pour répondre à la question, réponds par "Désolé, j'ignore la réponse à ta question."
+            Si l'entrée n'est pas une question tu agis comme un chatbot qui aide les clients.
+           
+            Tes réponses doivent être courtes mais contiennent suffisamment de détails.
+            """
+
+        system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+
+        # Human question prompt
+        human_template = "Réponds à l'entrée du client suivante : {question}"
+        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+        chat_prompt = ChatPromptTemplate.from_messages(
+            [system_message_prompt, human_message_prompt]
+        )
+
+        chain = LLMChain(llm=chat, prompt=chat_prompt)
+
+        try:
+            response = chain.run(question=query, docs=docs_page_content)
+
+            return response
+        except:
+            return None
+        
+    
